599e7f6ba4dad2c851dec1f7818a2e5f24779554
Steve <steve.lum@gmail.com>
2014-03-09 14:31:26 -0700

    trying to fix issues with undefined method disk_existing_method error

--- resources/ebs_raid.rb
+++ resources/ebs_raid.rb
@@ -31,5 +31,5 @@ attribute :filesystem_options,    :default => "rw,noatime,nobootwait"
 attribute :snapshots,             :default => []
 attribute :disk_type,             :kind_of => String, :default => 'standard'
 attribute :disk_piops,            :kind_of => Integer, :default => 0
-attribute :existing_raid,         :kind_of => [ TrueClass, FalseClass ]
+attribute :disk_existing_raid,         :kind_of => [ TrueClass, FalseClass ]
 
